e6fcc94647868e6e0f7dfa73040a1ac0444ee79f
Amit <amittambulkar104@gmail.com>
2024-10-12 18:48:06 +0530

    chore: update hero section

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import { Button } from "@/components/ui/button";
 import { LinkedinLogo, XLogo } from "@phosphor-icons/react/dist/ssr";
 import Image from "next/image";
@@ -6,45 +7,27 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <header className="mx-[4.5rem] my-6 flex items-center justify-between">
+      <Header />
+      <section
+        id="hero"
+        className="mx-auto flex max-w-3xl flex-col items-center justify-center text-center"
+      >
+        <h1 className="mb-6 flex items-center justify-center gap-8 text-6xl">
+          <span>Design</span>
+          <span>Develop</span>
+          <span>Deploy</span>
+        </h1>
+        <p className="mb-8 w-[80%] text-base text-slate-600">
+          I design and develop web applications and deploy them in weeks not
+          months (Focused on Frontend Development)
+        </p>
         <div className="flex items-center gap-4">
-          <Image
-            width={32}
-            height={32}
-            src="/amit.png"
-            alt="Amit Tambulkar photo"
-          />
-          <p className="text-sm">Amit Tambulkar</p>
+          <Button variant="secondary">
+            Know More
+          </Button>
+          <Button>Let&apos;s Connect</Button>
         </div>
-        <div className="flex items-center gap-8">
-          <nav>
-            <ul className="flex items-center gap-8 text-sm">
-              <li>Portfolio</li>
-              <li>Skills</li>
-              <li>About</li>
-            </ul>
-          </nav>
-          <div className="flex items-center gap-4">
-            <Link
-              href="https://www.linkedin.com/in/amittambulkar/"
-              target="_blank"
-              className="transition-colors delay-75 ease-in-out hover:text-blue-600"
-            >
-              <LinkedinLogo size={22} />
-            </Link>
-            <Link
-              href="https://x.com/attambulkar"
-              target="_blank"
-              className="transition-colors delay-75 ease-in-out hover:text-blue-600"
-            >
-              <XLogo size={22} />
-            </Link>
-          </div>
-          <div>
-            <Button>Let&apos;s Connect</Button>
-          </div>
-        </div>
-      </header>
+      </section>
     </div>
   );
 }
