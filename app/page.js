fd8a7b5c979f2b2556beb35f7de44f8cb091ad9d
Amit <amittambulkar104@gmail.com>
2024-10-13 10:07:13 +0530

    chore: update skills section

--- app/page.js
+++ app/page.js
@@ -2,10 +2,6 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Portfolio from "@/components/Portfolio";
 import Skills from "@/components/Skills";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import { ArrowRight } from "@phosphor-icons/react/dist/ssr";
-import Image from "next/image";
 
 export default function Home() {
   return (
